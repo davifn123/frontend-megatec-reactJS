629afba1b6dca5739db256fd86b3fdce1378edc2
fariasdavi1999 <you@example.com>
2022-08-10 21:31:16 -0300

    Integrando axios

--- src/pages/CadastrarProdutos.js
+++ src/pages/CadastrarProdutos.js
@@ -1,5 +1,8 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
+
+import { useForm } from 'react-hook-form';
 
 //estilos e imagens
 import '../styles/cadastrarProdutos.css'
@@ -15,17 +18,32 @@ function CadastrarProdutos() {
     const [descricaoProduto, setDescricaoproduto] = useState('');
     const [cateProduto, setCateproduto] = useState('');
     const [precoProd, setPrecoprod] = useState('');
+    const [cnpjForne, setCnpjForne] = useState('');
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
 
-    function submitProduto() {
-        if (nomeProduto === '' || descricaoProduto === '' || cateProduto === '' || precoProd === '') {
-            alert('CADASTRO DO PRODUTO INVÁLIDO');
-        } else {
-            alert('CADASTRO DO PRODUTO EFETUADO');
+    // function submitProduto() {
+    //     if (nomeProduto === '' || descricaoProduto === '' || cateProduto === '' || precoProd === '') {
+    //         alert('CADASTRO DO PRODUTO INVÁLIDO');
+    //     } else {
+    //         alert('CADASTRO DO PRODUTO EFETUADO');
+
+    //     }
+
+    // }
+
+
+    const addPost = data => axios.post("https://megatec-store.herokuapp.com/api/produtos", data)
+        .then(() => {
+            alert("Incluido com sucesso")
             navigate('/listarProdutos');
-        }
+        })
+        .catch(() => {
+            alert("Registro não incluido")
+        })
 
-    }
+
 
     return (
         <div className="containerCadastrarProdutos">
@@ -34,36 +52,47 @@ function CadastrarProdutos() {
                 <div className="cadastrarProdutoForm">
 
                     <h1>Inserir Novo Produto</h1>
-                    <form>
+                    <form onSubmit={handleSubmit(addPost)}>
                         <div className="input-containerproduto">
 
-                            <input type="text" name="productname" placeholder="Nome do produto"
+                            <input type="text" name="cnpj_fornecedor"
+                                {...register("cnpj_fornecedor")} placeholder="CNPJ do Fornecedor"
+                                value={cnpjForne} onChange={(e) => setCnpjForne(e.target.value)} required />
+
+                        </div>
+                        <div className="input-containerproduto">
+
+                            <input type="text" name="nome_produto"
+                                {...register("nome_produto")} placeholder="Nome do produto"
                                 value={nomeProduto} onChange={(e) => setNomeproduto(e.target.value)} required />
 
                         </div>
                         <div className="input-containerproduto">
 
-                            <input type="text" name="productcat" placeholder="Categoria do produto (Cadeira / teclado etc...)"
+                            <input type="number" name="quantidade_produto"
+                                {...register("quantidade_produto")} placeholder="Quantidade..."
                                 value={cateProduto} onChange={(e) => setCateproduto(e.target.value)} required />
 
                         </div>
 
                         <div className="input-containerproduto">
 
-                            <input type="number" name="productdesc" placeholder="Preço"
+                            <input type="number" name="valor_produto"
+                                {...register("valor_produto")} placeholder="Preço"
                                 value={precoProd} onChange={(e) => setPrecoprod(e.target.value)} required />
 
                         </div>
                         <div className="input-containerproduto">
 
-                            <input type="text" name="productdesc" placeholder="Descrição do produto"
+                            <input type="text" name="obs_produto"
+                                {...register("obs_produto")} placeholder="Descrição do produto"
                                 value={descricaoProduto} onChange={(e) => setDescricaoproduto(e.target.value)} required />
 
                         </div>
 
 
                         <div className="button-containerproduto">
-                            <button onClick={submitProduto} type="submit" >Cadastrar</button>
+                            <button type="submit" >Cadastrar</button>
                         </div>
                     </form>
                 </div>
